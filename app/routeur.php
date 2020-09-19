6918207dbeb03175333484f13ba07a3b787d9488
laura <lauralekeu13@gmail.com>
2020-09-19 16:27:10 +0200

    Nettoyage du routeur
    Ajout du sous-routeur pour les posts

--- app/routeur.php
+++ app/routeur.php
@@ -5,30 +5,29 @@
 
 */
 
-// PAGE CONTACT
-// PATTERN: /contact.html
-// CTRL: -
-// ACTION:  -
+
 if (isset($_GET['contact'])):
+// ROUTE PAGE CONTACT
+  // PATTERN: /contact.html
+  // CTRL: -
+  // ACTION:  -
   $title = TITRE_CONTACT;
   ob_start(); // pas besoin de mettre global car on est dans le routeur, au même niveau que les parametres
   include_once '../app/vues/template/partials/_contact.php';
   $content = ob_get_clean();
 
 
-// ROUTE D'UN POST
-// PATTERN: /index.php?postId=x
-// CTRL: postsControleur
-// ACTION:  showAction
-elseif (isset($_GET['postId'])):
-  include_once '../app/controleurs/postsControleur.php';
-  \App\Controleurs\PostsControleur\showAction($connexion, $_GET['postId']);
+
+elseif (isset($_GET['posts'])):
+// ROUTES DES POSTS
+  include_once '../app/routeurs/postsRouteur.php';
+
 
 else:
-// ROUTE PAR DEFAUT (Liste des 10 derniers posts)
-// PATTERN: /
-// CTRL: postsControleur
-// ACTION:  indexAction
+// ROUTE PAR DEFAUT
+  // PATTERN: /
+  // CTRL: postsControleur
+  // ACTION:  indexAction
 include_once '../app/controleurs/postsControleur.php';
   \App\Controleurs\PostsControleur\indexAction($connexion);
   endif;
--- app/vues/template/partials/_aside.php
+++ app/vues/template/partials/_aside.php
@@ -7,10 +7,10 @@
 ?>
 
 <aside class="single_sidebar_widget search_widget">
-    <form action="#">
+    <form action="posts/search">
         <div class="form-group">
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder='Search Keyword'
+                <input type="text" name="search" class="form-control" placeholder='Search Keyword'
                     onfocus="this.placeholder = ''"
                     onblur="this.placeholder = 'Search Keyword'">
                 <div class="input-group-append">
